7d0760680e71896856660d71abd2f7ad41d75fa4
daniahih <daniahih310@gmail.com>
2024-09-04 20:21:45 +0300

    added intro to js

---
+++ js/introTojs/script.js
@@ -0,0 +1,26 @@
+// console.log("mohammad");
+// console.log("mohammad");
+// console.log(`mohammad`);
+// let /  var . const
+
+let name = "duryed";
+name = "shadi";
+console.log(name);
+
+// my name is dania +
+// ,
+// ``
+
+//string to write it
+
+// number
+//string
+//symle
+//char
+//boolean
+//null
+//big int
+//undifind
+//nan
+//date
+//enum
